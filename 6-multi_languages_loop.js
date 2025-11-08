5fe90c25159fe2a6b270cca9487e3a52e50df386
Kelvin Bamfo <kelvinappiah765@gmail.com>
2025-11-08 23:13:52 +0200

    Updated loop to languages

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,2 +1,19 @@
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-console.log(lines.join("\n"));
+const temp = [];
+
+for (let i = 0; i < lines.length; i++) {
+  temp[i] = lines[i];
+}
+
+let result = "";
+let j = 0;
+
+while (j < temp.length) {
+  result += temp[j];
+  if (j < temp.length - 1) {
+    result += "\n";
+  }
+  j++;
+}
+
+console.log(result);
